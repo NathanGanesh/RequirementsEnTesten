72a1709077f928ecd9ac5796ca8ed8d2bd882381
nathanganesh <nathangemieee@gmail.com>
2020-06-26 21:25:41 +0200

    good path timesheet

--- src/org/flyveleder/model/Timesheet.java
+++ src/org/flyveleder/model/Timesheet.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class Timesheet {
 
 
-    private final HashMap<String,TimesheetItem> timeSheet;
+    private final HashMap<String, TimesheetItem> timeSheet;
 
     public Timesheet() {
         timeSheet = new HashMap<>();
@@ -17,64 +17,85 @@ public class Timesheet {
 
     /**
      * Add an timesheet item to the ledger
+     *
      * @param item
-     * @throws FlyvelederModelException
-     *
-     * pre-condition: The item should not cause a double entry for the trafficcontroller. e.g.
-     * if the trafficcontroller is working on 2020-10-1 from 12:00 to 18:00 an attempt to add
-     * an item for this trafficcontroler for 2020-10-1 from 17:00 to 21:00 should fail with a
-     * FlyvelederModelException!
-     *
-     * post-condition: the new item should be appendend in the ledger.
+     * @throws FlyvelederModelException pre-condition: The item should not cause a double entry for the trafficcontroller. e.g.
+     *                                  if the trafficcontroller is working on 2020-10-1 from 12:00 to 18:00 an attempt to add
+     *                                  an item for this trafficcontroler for 2020-10-1 from 17:00 to 21:00 should fail with a
+     *                                  FlyvelederModelException!
+     *                                  <p>
+     *                                  post-condition: the new item should be appendend in the ledger.
      */
     public void addItem(TimesheetItem item) throws FlyvelederModelException {
-        //todo: make sure businesss rules are correctly implemented
-        timeSheet.put(item.getId(),item);
+        boolean doubleEntry = false;
+        HashMap<String, TimesheetItem> timesheetItemHashMap = getTimeSheetForUser(item.getTrafficController().getId());
+
+        for (TimesheetItem value : timesheetItemHashMap.values()) {
+            //if there is another value for an already existing item it means double planned
+            if (value.getStart().isBefore(item.getEnd()) && value.getEnd().isAfter(item.getStart())) {
+                doubleEntry = true;
+                break;
+            }
+
+        }
+        assert !doubleEntry : new FlyvelederModelException("User is planned double which cant");
+
+        timeSheet.put(item.getId(), item);
     }
 
     public void removeItem(TimesheetItem item) throws FlyvelederModelException {
+        assert timeSheet.containsValue(item) : "Key doesnt exist";
         timeSheet.remove(item.getId());
     }
 
     public void removeItem(String itemid) throws FlyvelederModelException {
+        assert itemid != null : "Null naam";
+        assert !itemid.isEmpty() : "Null naam";
+        assert timeSheet.containsKey(itemid) : "Key doesnt exist";
         timeSheet.remove(itemid);
     }
 
 
     public TimesheetItem getTimeSheetItem(String id) {
+        assert id != null : "Null naam";
+        assert !id.isEmpty() : "Null naam";
+        assert timeSheet.containsKey(id) : "Key doesnt exist";
         return timeSheet.get(id);
     }
 
     /**
-     *
      * @return all items in the time sheet.
      * todo: This method is not save...
      * Explain why this is the case and change it so is is save
      */
-    public HashMap<String,TimesheetItem> getItems() {
+    public HashMap<String, TimesheetItem> getItems() {
         return timeSheet;
     }
 
 
     /**
-     *
      * @param userid
      * @return all the timesheet items for a specific user
      * @throws FlyvelederModelException
      */
-    public HashMap<String,TimesheetItem> getTimeSheetForUser(String userid) throws FlyvelederModelException {
+    public HashMap<String, TimesheetItem> getTimeSheetForUser(String userid) throws FlyvelederModelException {
+        // TODO: 26-6-2020 assert used id --> so it assert to the hashmap
+        assert userid != null : "Null naam";
+        assert !userid.isEmpty() : "Null naam";
         return (HashMap<String, TimesheetItem>) timeSheet.entrySet().stream()
                 .filter(u -> u.getValue().getTrafficController().getId().equals(userid))
                 .collect(Collectors.toMap(u -> u.getKey(), u -> u.getValue()));
     }
 
     /**
-     *
      * @param projectid
      * @return all the timesheet items for a specific project
      * @throws FlyvelederModelException
      */
-    public HashMap<String,TimesheetItem> getTimeSheetForProject(String projectid) throws FlyvelederModelException {
+    public HashMap<String, TimesheetItem> getTimeSheetForProject(String projectid) throws FlyvelederModelException {
+        // TODO: 26-6-2020 assert used id --> so it assert to the hashmap
+        assert projectid != null : "Null naam";
+        assert !projectid.isEmpty() : "Null naam";
         return (HashMap<String, TimesheetItem>) timeSheet.entrySet().stream()
                 .filter(u -> u.getValue().getProject().getId().equals(projectid))
                 .collect(Collectors.toMap(u -> u.getKey(), u -> u.getValue()));
---
+++ test/org/flyveleder/model/TimesheetTest.java
@@ -0,0 +1,80 @@
+package org.flyveleder.model;
+
+import org.flyveleder.exceptions.FlyvelederModelException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TimesheetTest {
+
+    private User boss_manger;
+    private Timesheet timesheet;
+    private Project project;
+    private User customer;
+//    private User
+
+    private User trafficController;
+    private TimesheetItem timesheetItem1;
+    private TimesheetItem timesheetItem2;
+    private TimesheetItem timesheetItem3;
+    private HashMap<String,TimesheetItem> timesheetItemHashMap;
+
+    @BeforeAll
+    public void startUp() throws FlyvelederModelException {
+        boss_manger = new User("bossman", User.MANAGER, null, null);
+        customer = new User("customer", User.CUSTOMER, boss_manger, null);
+        project = new Project("Test", customer, timesheet);
+
+
+        timesheetItem2 = new TimesheetItem(trafficController, project, LocalTime.of(14, 0), LocalTime.of(13, 0), LocalDate.now());
+        timesheetItem3 = new TimesheetItem(trafficController, project, LocalTime.of(11, 30), LocalTime.of(17, 30), LocalDate.now());
+    }
+
+    @Test
+    public void goodTimeSheetTest() throws FlyvelederModelException {
+        timesheet = new Timesheet();
+        timesheetItem1 = new TimesheetItem(boss_manger, project, LocalTime.of(11, 0), LocalTime.of(17, 0), LocalDate.now());
+        timesheet.addItem(timesheetItem1);
+        assertEquals(timesheet.getItems().size(), 1);
+        trafficController = new User("trafficcontroller", User.TRAFFICCONTROLLER, boss_manger, timesheet);
+
+
+        assertEquals(timesheet.getTimeSheetItem("3"), timesheetItem1);
+        //timesheet item
+        timesheet.removeItem(timesheetItem1);
+        assertEquals(timesheet.getItems().size(), 0);
+
+       timesheet.addItem(timesheetItem1);
+        assertEquals(timesheet.getItems().size(), 1);
+       //timesheet item id
+       timesheet.removeItem(timesheetItem1.getId());
+        assertEquals(timesheet.getItems().size(), 0);
+
+        timesheet.addItem(timesheetItem1);
+        Project project = new Project("corona", boss_manger, timesheet);
+        assertEquals(timesheet.getTimeSheetForProject("1").size(),1);
+
+        assertEquals(timesheet.getTimeSheetForProject("1").get("3"), timesheetItem1);
+
+//        assertEquals(timesheet.getTimeSheetForProject("1").get());
+//        System.out.println(timesheet.getTimeSheetForUser(trafficController.getId()).get("1"));
+//        TimesheetItem timesheetItem = timesheet.getTimeSheetForUser(trafficController.getId()).get("1");
+//        assertEquals(timesheetItem, timesheetItem1);
+
+    }
+
+    /**
+     * Bad weather tests for the timesheet class
+     */
+    @Test
+    public void testBadUser() {
+
+    }
+}
